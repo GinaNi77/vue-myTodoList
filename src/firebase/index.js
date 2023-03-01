5ba62e7884df6c74656b1b5c29c7367c32242bd6
Nigina Gafurova <nigina.gafurova.2001@mail.ru>
2023-03-01 12:38:44 +0300

    firebase install

---
+++ src/firebase/index.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0aIUHlIki6DnNNWbBbaaq0bAVyZpkSJc",
+  authDomain: "mytodolist-6a1fc.firebaseapp.com",
+  projectId: "mytodolist-6a1fc",
+  storageBucket: "mytodolist-6a1fc.appspot.com",
+  messagingSenderId: "265328430672",
+  appId: "1:265328430672:web:e20ac59dc253c020828b55"
+};
+
+
+const app = initializeApp(firebaseConfig);
