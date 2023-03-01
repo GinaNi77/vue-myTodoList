75e43b37a0e9df51411baa8f3626aeeba680b786
Nigina Gafurova <nigina.gafurova.2001@mail.ru>
2023-03-01 12:29:39 +0300

    todo done

--- src/App.vue
+++ src/App.vue
@@ -17,13 +17,14 @@
     </div>
     </form>
     <div v-for="todo in todos" :key="todo.id"
-    class="card mb-5">
+    class="card mb-5" :class="{'has-background-success-light' : todo.done}">
         <div class="card-content">
             <div class="content">
                  <div class="columns is-mobile is-vcentered">
-                    <div class="column">{{todo.content}}</div>
+                    <div class="column" :class="{'has-text-success line-through' : todo.done}">{{todo.content}}</div>
                     <div class="column is-5 has-text-right">
-                        <button class="button has-background-success has-text-white mr-2">&check;</button>
+                        <button class="button has-text-white mr-2"
+                        :class="todo.done ? 'is-success' : 'is-light'" @click="togglerDone(todo.id)">&check;</button>
                         <button class="button has-background-danger has-text-white" @click="deleteTodo(todo.id)">&cross;</button>    
                     </div>
                 </div>
@@ -51,11 +52,11 @@ const todos = ref([
     //     content: 'Youre excused',
     //     done: false    
     // },
-    // {
-    //     id:'id3',
-    //     content: 'And Im not your brah',
-    //     done: false    
-    // }
+    {
+        id:'id3',
+        content: 'And Im not your brah',
+        done: true    
+    }
 ]);
 
 //add todo
@@ -71,11 +72,18 @@ const addTodo = ()=>{
     todos.value.unshift(newTodo)
     newtodoContent.value =""
 }
+//delete todo
 
 const deleteTodo = (id)=>{
     todos.value = todos.value.filter((todo)=> todo.id!==id)
 }
 
+//todo done
+
+const togglerDone = (id)=>{
+    const index = todos.value.findIndex(todo => todo.id === id)
+    todos.value[index].done = !todos.value[index].done
+}
 </script>
 
 
@@ -87,5 +95,9 @@ const deleteTodo = (id)=>{
     padding: 20px;
     max-width: 400px;
     margin: 0 auto;
+}
+
+.line-through{
+    text-decoration: line-through;
 }
 </style>
